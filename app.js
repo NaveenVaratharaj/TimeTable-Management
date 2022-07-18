97c3921cc9ecbf997ffa040a17d15259ab0f5d74
Naveen Varatharaj <naveenvm.cs20@bitsathy.ac.in>
2022-07-18 20:50:13 +0530

    Home page UI implemented

--- app.js
+++ app.js
@@ -1,9 +1,26 @@
+// Including Dotenv
+if(process.env.Node !== 'production'){
+    require("dotenv").config()
+}
+
+// Required Dependencies
 const express = require("express")
-
 const app = express();
+const mongoose = require("mongoose")
+const indexRouter = require("./routes/index")
 
-app.get("/", (req,res) =>{
-    res.send("123")
+// Middleware
+app.set("view engine", "ejs")
+app.use(express.static('public'))
+app.use("/", indexRouter)
+
+// Db connection
+mongoose.connect(process.env.DATABASE_URL, {useNewUrlParser: true} , () =>{
+    console.log("Database Connected!")
 })
 
-app.listen(8000)
+
+// Port
+app.listen(process.env.PORT || 4000, ()=>{
+    console.log("App Started at the ", process.env.PORT)
+})
